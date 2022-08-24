626ef29557d6f148419ed9ed457c888be1a1ede8
christhodo <102981895+christhodo@users.noreply.github.com>
2022-08-24 10:31:56 -0700

    initial commit

---
+++ apps/espresso-recipes/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'espresso-recipes-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'espresso-recipes';
+}
